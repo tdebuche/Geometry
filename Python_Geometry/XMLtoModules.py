474904382bb8ebdb3399fe3456a2f22311598e51
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-19 17:44:40 +0200

    Update XMLtoModules.py

--- Python_Geometry/XMLtoModules.py
+++ Python_Geometry/XMLtoModules.py
@@ -25,9 +25,10 @@ def read_xml(file):
                 v  = int(module_element.get('v'))
                 x = float(module_element.get('x'))
                 y = float(module_element.get('y'))
+                TCcount = int(module_element.get('TCcount'))
                 verticesX,verticesY = vertices(module_element.get('Vertices'))
                 if Silliciumorscintillateur(int_id) == 'sillicium':
-                    Modules[layer-1].append({'id':module_id,'u':u,'v':v,'irot':irot,'verticesX' :verticesX,'verticesY' :verticesY})
+                    Modules[layer-1].append({'id':module_id,'u':u,'v':v,'irot':irot,'TCcount':TCcount,'verticesX' :verticesX,'verticesY' :verticesY})
 
     layer = 0
     for layer_element in root.findall('.//Plane'):
@@ -40,9 +41,10 @@ def read_xml(file):
                 v  = int(tile_element.get('v'))
                 x = float(tile_element.get('x'))
                 y = float(tile_element.get('y'))
+                TCcount = int(module_element.get('TCcount'))
                 irot = 999
                 verticesX,verticesY = vertices(tile_element.get('Vertices'))
-                Modules[layer-1].append({'id':tile_id,'u':u,'v':v,'irot':irot,'verticesX' :verticesX,'verticesY' :verticesY})
+                Modules[layer-1].append({'id':tile_id,'u':u,'v':v,'irot':irot,'TCcount':TCcount,'verticesX' :verticesX,'verticesY' :verticesY})
     return Modules
 
 def Silliciumorscintillateur(id):
