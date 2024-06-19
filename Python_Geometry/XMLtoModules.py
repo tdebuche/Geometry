1cc100fe531e634570cc93b96e66597e0c7a0183
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-19 14:09:28 +0200

    Update XMLtoModules.py

--- Python_Geometry/XMLtoModules.py
+++ Python_Geometry/XMLtoModules.py
@@ -17,6 +17,7 @@ def read_xml(file):
         for motherboard_element in layer_element.findall('.//Motherboard'):
             for module_element in motherboard_element.findall('.//Module'):
                 int_id = int(module_element.get('id'),16)
+				irot = int(module_element.get('irot'))
                 module_id = str(f'{int_id : 08x}')
                 u = int(module_element.get('u'))
                 v  = int(module_element.get('v'))
