490d61007c2babb0afaadaabcd8cbad4fc02f0e7
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-19 13:09:39 +0200

    Update Geometric_tools.py

--- Python_Geometry/Geometric_tools.py
+++ Python_Geometry/Geometric_tools.py
@@ -1,4 +1,23 @@
+import numpy as np
+import awkward as ak
 
-def STCs_single_Module
 
-def TCs_single_Module
+def item_list(json,item):
+  L = []
+  with open('your_file.json', 'r') as file:
+    data = json.load(file)[0]
+  for module_idx in range(len(data)):
+    if item =="id":
+      L.append(data[module_idx]['id'])
+    if item =="irot":
+      L.append(data[module_idx]['irot'])
+    if item =="uv":
+      L.append([data[module_idx]['u'],data[module_idx]['v']])
+    if item =="vertices":
+      L.append([data[module_idx]['verticesX'],data[module_idx]['verticesY']])
+    return L
+    
+  
+#def STCs_single_Module
+
+#def TCs_single_Module
