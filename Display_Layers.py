1a39fb876a822e72543dbfd20c310b18fde56cb5
tdebuche <thomas.de-buchere-de-lepinois@polytechnique.edu>
2024-06-19 14:04:53 +0200

    updates

--- Display_Layers.py
+++ Display_Layers.py
@@ -9,17 +9,30 @@ from Python_Geometry.Geometric_tools import item_list
 parser = argparse.ArgumentParser()
 parser.add_argument("Layer", help="Layer to display",type=int)
 parser.add_argument("--UV",default = 'no', help="With or without UV")
+parser.add_argument("--irot",default = 'no', help="With or without rot")
 parser.add_argument("--STCs",default = 'no', help="With or without STCs")
 parser.add_argument("--Record",default = 'no', help="Record all layers")
 args = parser.parse_args()
 Layer = args.Layer
 
 Module_Vertices = item_list('Python_Geometry/src/Modules.json','vertices')
+Module_UV = item_list('Python_Geometry/src/Modules.json','uv')
+Module_irot = item_list('Python_Geometry/src/Modules.json','irot')
+
+
 plt.figure(figsize = (12,8))
 plt.title(label =  'Layer '+str(Layer))
 plt.xlabel('x (mm)')
 plt.ylabel('y (mm)')                            
 for module_idx in range(len(Module_Vertices)):
-    Xvertices= Module_Vertices[module_idx][0] + Module_Vertices[module_idx][0][0]
-    Yvertices= Module_Vertices[module_idx][1] + Module_Vertices[module_idx][1][0]
+    Xvertices= Module_Vertices[module_idx][0] +[Module_Vertices[module_idx][0][0]]
+    Yvertices= Module_Vertices[module_idx][1] + [Module_Vertices[module_idx][1][0]]
+    x,y = np.sum(np.array(Module_Vertices[module_idx][0]))/len(Module_Vertices[module_idx][0]),np.sum(np.array(Module_Vertices[module_idx][1]))/len(Module_Vertices[module_idx][1]) 
+    if args.UV == "yes":
+	u,v = Module_UV[module_idx][0],Module_UV[module_idx][1]
+ 	plt.annotate("("+str(u)+","str(v)+")",(x-60,y-10))
+    if args.irot == "yes":
+	rot = Module_irot[module_idx]
+	plt.annotate(rot,(x-60,y-10))
     plt.plot(Xvertices,Yvertices,color = "black")
+plt.show()
