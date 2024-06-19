25a66e6ac84e1161804cb3eb09d097fbfc0b6775
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-19 12:50:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from TSVtoXML.Geometry import TsvToXML
+import json
+from Python_Geometry/XMLtoModules import read_xml
 
 SrcFile = "TSVtoXML/src/v15.5/geometry_sipmontile.hgcal.txt"
 
-GeometryFile = TsvToXML( SrcFile , "Geometry.xml" )
+#GeometryFile = TsvToXML( SrcFile , "Geometry.xml" )
+
+
+jsonlist = read_xml("Geometry.xml" )
+with open('Python_Geometry/src/Modules.json', 'w') as mon_fichier:
+    json.dump(jsonlist, mon_fichier)
