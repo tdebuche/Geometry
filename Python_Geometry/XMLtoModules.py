29642c285f79bed4783a230413470c34e16039c2
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-19 14:09:54 +0200

    Update XMLtoModules.py

--- Python_Geometry/XMLtoModules.py
+++ Python_Geometry/XMLtoModules.py
@@ -38,7 +38,7 @@ def read_xml(file):
                 v  = int(tile_element.get('v'))
                 x = float(tile_element.get('x'))
                 y = float(tile_element.get('y'))
-		    irot = 999
+		irot = 999
                 verticesX,verticesY = vertices(tile_element.get('Vertices'))
                 Modules[layer-1].append({'id':tile_id,'u':u,'v':v,'irot':irot,'verticesX' :verticesX,'verticesY' :verticesY})
     return Modules
