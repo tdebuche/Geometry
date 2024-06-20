8f20f4537536932b825c136738438f88038e63d8
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-20 12:07:36 +0200

    Update Final_Geometry.py

--- Python_Geometry/Final_Geometry.py
+++ Python_Geometry/Final_Geometry.py
@@ -1,4 +1,34 @@
-def Layer_with_STCs
+from Python_Geometry.Geometric_tools import single_module_STCs
 
 
-def Layer_with_TCs
+def One_Layer_STCs(modules):
+  STCs = []
+  for module_idx in range(len(modules)):
+    module =  modules[module_idx]
+    if module['TCcount'] != 999 :
+      vertices =  [module['verticesX'],module['verticesY']]
+      vertices[0] = vertices[0] + vertices[0][0:6-len(vertices[0])]
+      vertices[1] = vertices[1] + vertices[1][0:6-len(vertices[1])]
+      module_STCs =  single_module_STCs(vertices,module['irot'],module['TCcount'])
+      for STC_idx in range(len(module_STCs)):
+        STCs.append({"id": module['id'], "u": module['u'], "v": module['v'],"index": STC_idx, "verticesX": module_STCs[STC_idx][0] , "verticesY": module_STCs[STC_idx][1]})
+  return(STCs)
+
+def STC_geometry(jsonfile):
+  STCs = []
+  with open(jsonfile,'r') as file:
+    modules = json.load(file)
+  for layer in range(47):
+    layer_modules = modules[layer-1]
+    if layer > 26:
+      STCs.append(One_Layer_STCs(modules))
+  with open('Python_Geometry/src/STCs.json', 'w') as file:
+    json.dump(STCs, file)
+  
+
+
+
+
+
+
+#def Layer_with_TCs
