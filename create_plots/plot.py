3b7518592a06c94b79fc23b91f906ccc405c9faa
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 17:47:02 +0200

    Update plot.py

--- create_plots/plot.py
+++ create_plots/plot.py
@@ -5,12 +5,11 @@ import matplotlib.pyplot as plt
 def plot_layer(args):
 	Layer = args.Layer
 
-	Module_Vertices = item_list('Python_Geometry/src/Modules.json','vertices',Layer)
-	Module_UV = item_list('Python_Geometry/src/Modules.json','uv',Layer)
-	Module_irot = item_list('Python_Geometry/src/Modules.json','irot',Layer)
-	Module_TCcount = item_list('Python_Geometry/src/Modules.json','TCcount',Layer)
-	STC_Vertices = item_list('Python_Geometry/src/STCs.json','vertices',Layer)
-	STC_indices = item_list('Python_Geometry/src/STCs.json','index',Layer)
+	Module_Vertices = item_list('src/'+args.Modmap_version+'/Modules.json','vertices',Layer)
+	Module_UV = item_list('src/'+args.Modmap_version+'/Modules.json','uv',Layer)
+	Module_irot = item_list('src/'+args.Modmap_version+'/Modules.json','irot',Layer)
+	STC_Vertices = item_list('src/'+args.Modmap_version+'/STCs.json','vertices',Layer)
+	STC_indices = item_list('src/'+args.Modmap_version+'/STCs.json','index',Layer)
 
 	plt.figure(figsize = (12,8))
 	plt.title(label =  'Layer '+str(Layer))
@@ -29,16 +28,16 @@ def plot_layer(args):
 		vertices = Module_Vertices[module_idx]
 		Xvertices= vertices[0] +[vertices[0][0]]
 		Yvertices= vertices[1] +[vertices[1][0]]
+		plt.plot(Xvertices,Yvertices,color = "black")
+		
 		x,y = np.sum(np.array(vertices[0]))/len(vertices[0]),np.sum(np.array(vertices[1]))/len(vertices[0])
-		plt.scatter((x+Xvertices[0])/2,(y+Yvertices[0])/2 ,color ="red")
-	if args.UV == "yes":
-	    u,v = Module_UV[module_idx][0],Module_UV[module_idx][1]
-	    plt.annotate("("+str(u)+","+str(v)+")",(x-60,y-10),size =  '8')
-	if args.irot == "yes":
-	    plt.annotate(str(irot),(x-60,y-10),size =  '8')
-	plt.plot(Xvertices,Yvertices,color = "black")
+		if args.UV == "yes":
+	    	u,v = Module_UV[module_idx][0],Module_UV[module_idx][1]
+	    	plt.annotate("("+str(u)+","+str(v)+")",(x-60,y-10),size =  '8')
+		if args.irot == "yes":
+	    	plt.annotate(str(irot),(x-60,y-10),size =  '8')
 
-plt.show()
+	plt.show()
 	
 	
 
