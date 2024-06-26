41a273f1f6049c1dd54506b718d2ebd98464cea6
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 10:33:35 +0200

    Update Display_Layers_with_event.py

--- Display_Layers_with_event.py
+++ Display_Layers_with_event.py
@@ -1,2 +1,15 @@
 from add_events.read_events import provide_events
 from add_events.plot import plot_TCs_of_multiple_events
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("Layer", help="Layer to display",type=int)
+parser.add_argument("--UV",default = 'no', help="With or without UV")
+parser.add_argument("--irot",default = 'no', help="With or without rot")
+parser.add_argument("--STCs",default = 'no', help="With or without STCs")
+parser.add_argument("--Record",default = 'no', help="Record all layers")
+args = parser.parse_args()
+
+
+events = provide_events(1)
+plot_TCs_of_multiple_events(args,events)
