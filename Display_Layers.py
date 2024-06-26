fd9daca3cfdba9b177d2b5150237ab0aebc2710d
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 18:07:25 +0200

    Update Display_Layers.py

--- Display_Layers.py
+++ Display_Layers.py
@@ -5,7 +5,8 @@ from create_plots.plot import plot_layer_with_events,plot_layer
 
 parser = argparse.ArgumentParser()
 
-
+#version of the geometry 
+parser.add_argument("--Modmap_version",default = 'v13.1', help="Geometry version")
 
 #for the plot
 parser.add_argument("Layer", help="Layer to display",type=int)
@@ -13,8 +14,7 @@ parser.add_argument("--UV",default = 'no', help="With or without UV")
 parser.add_argument("--irot",default = 'no', help="With or without rot")
 parser.add_argument("--STCs",default = 'yes', help="With or without STCs")
 parser.add_argument("--events",default = 'no', help="With or without events")
-#version of the geometry 
-parser.add_argument("--Modmap_version",default = 'v13.1', help="Geometry version")
+
 args = parser.parse_args()
 
 if args.events == "yes":
