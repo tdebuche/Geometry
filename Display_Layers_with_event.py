126868d1a7e7dd4ff9752c52667f433274f1969b
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 14:39:19 +0200

    Update Display_Layers_with_event.py

--- Display_Layers_with_event.py
+++ Display_Layers_with_event.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("Layer", help="Layer to display",type=int)
 parser.add_argument("--UV",default = 'no', help="With or without UV")
 parser.add_argument("--irot",default = 'no', help="With or without rot")
-parser.add_argument("--STCs",default = 'no', help="With or without STCs")
+parser.add_argument("--STCs",default = 'yes', help="With or without STCs")
 parser.add_argument("--Record",default = 'no', help="Record all layers")
 args = parser.parse_args()
 
