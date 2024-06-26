ed31afbb5f1a402ad134ee2554b692ac49b8c454
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 12:37:25 +0200

    Update Display_Layers.py

--- Display_Layers.py
+++ Display_Layers.py
@@ -20,7 +20,7 @@ Module_UV = item_list('Python_Geometry/src/Modules.json','uv',Layer)
 Module_irot = item_list('Python_Geometry/src/Modules.json','irot',Layer)
 Module_TCcount = item_list('Python_Geometry/src/Modules.json','TCcount',Layer)
 STC_Vertices = item_list('Python_Geometry/src/STCs.json','vertices',Layer)
-STC_index = item_list('Python_Geometry/src/STCs.json','index',Layer)
+STC_indices = item_list('Python_Geometry/src/STCs.json','index',Layer)
 
 plt.figure(figsize = (12,8))
 plt.title(label =  'Layer '+str(Layer))
@@ -31,6 +31,8 @@ for STC_idx in range(len(STC_Vertices)):
 	Xvertices= vertices[0] +[vertices[0][0]]
 	Yvertices= vertices[1] +[vertices[1][0]]
 	plt.plot(Xvertices,Yvertices,color = "red")
+	x,y = np.sum(np.array(vertices[0]))/len(vertices[0]),np.sum(np.array(vertices[1]))/len(vertices[0])
+	plt.annotate(str(STC_indices[STC_idx]),(x-20,y-10),size =  '5')
 for module_idx in range(len(Module_Vertices)):
 	irot = Module_irot[module_idx]
 	TCcount = Module_TCcount[module_idx]
