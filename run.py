b10f98838c287f1100ba21cc4669c67f8518c5cf
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 12:00:34 +0200

    Update run.py

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ from Python_Geometry.Record_Geometry import STC_geometry
 
 SrcFile = "TSVtoXML/src/v13.1/geometry_sipmontile.hgcal.txt"
 GeometryFile = TsvToXML( SrcFile , "Geometry.xml" )
+TsvToXML( SrcFile , "Geometry.xml" )
 read_xml("Geometry.xml" )
 STC_geometry("Python_Geometry/src/Modules.json")
