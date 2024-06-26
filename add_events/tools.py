19618bcce4c1dc3f938c4205f515483aa92998f3
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-26 13:40:04 +0200

    Create tools.py

---
+++ add_events/tools.py
@@ -0,0 +1,39 @@
+
+
+
+def get_STC_index(HDorLD,cell_u,cell_v):
+  #LD
+  if HDorLD == 'LD':
+    if (cell_u <= cell_v) and (cell_u < 8):
+      return 0
+    if (cell_v > 7) and (cell_u >= 8):
+      return 1
+    if (cell_u > cell_v) and (cell_v <= 7):
+      return 1
+  #HD
+   if HDorLD == 'HD':
+    if (cell_v-cell_u >= 6) and (cell_u <= 5):
+      return 0
+    if (cell_v-cell_u >= 6) and (cell_u <= 11) and (cell_u > 5):
+      return 1
+    if (cell_v-cell_u >= 0) and (cell_v-cell_u < 6) and  (cell_u <= 11) and (cell_u > 5):
+      return 2
+    if (cell_v-cell_u >= 0) and (cell_v-cell_u < 6) and  (cell_u <= 5):
+      return 3
+    if (cell_v > 17) and  (cell_u > 17):
+      return 4
+    if (cell_v > 11) and (cell_v <= 17) and (cell_u > 17):
+      return 5
+    if (cell_v > 11) and (cell_v <= 17) and (cell_u > 11) and (cell_u <= 17):
+      return 6
+    if  (cell_v > 17) and (cell_u > 11) and (cell_u <= 17):
+      return 7
+    if (cell_v-cell_u < -6) and (cell_v <= 5):
+      return 8
+    if (cell_v-cell_u < 0) and (cell_v-cell_u >= -6) and (cell_v <= 5):
+      return 9
+    if (cell_v-cell_u < 0) and (cell_v-cell_u >= -6) and (cell_v <= 11) and (cell_v > 5):
+      return 10
+    if (cell_v-cell_u < -6) and (cell_v <= 11) and (cell_v > 5):
+      return 11
+    
